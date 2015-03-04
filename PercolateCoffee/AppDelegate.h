de4dde5cffbfea709e2f61a14bce889723fa1a15
DanielHabib <d.g.habib7@gmail.com>
2015-03-04 08:33:30 -0500

    Initial Commit

---
+++ PercolateCoffee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PercolateCoffee
+//
+//  Created by Daniel Habib on 3/4/15.
+//  Copyright (c) 2015 d.g.habib7@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
